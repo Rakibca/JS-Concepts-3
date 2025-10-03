07356508de79e83e2a97847b3f3382efc2f4c177
Rakibul Islam <rakib.ca@gmail.com>
2025-10-02 23:51:31 -0600

    code update

---
+++ 82_Object_assign().js
@@ -0,0 +1,19 @@
+const studentDetails = {
+  firstName: 'janaka',
+  lastName: 'siriwardena',
+  age: 28,
+  country: 'sri lanka',
+  email: 'j.siri@totalinternet.com',
+  discordUsername: 'JS1',
+};
+
+const studentDetailsCopy = {};
+
+Object.assign(studentDetailsCopy, studentDetails);
+
+console.log(studentDetailsCopy); // {firstName: 'janaka', lastName: 'siriwardena', age: 28, country: 'sri lanka', email: 'j.siri@totalinternet.com', discordUsername: 'JS1'}
+
+/*
+Challenge:
+    1. Prove that what we have created here is a shallow copy.
+*/
