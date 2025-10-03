ccc3df0d9ac65eaf0af39ec9a30ff64cf05c7ec2
Rakibul Islam <rakib.ca@gmail.com>
2025-10-02 23:59:33 -0600

    code update

--- 82_Object_assign().js
+++ 82_Object_assign().js
@@ -5,6 +5,7 @@ const studentDetails = {
   country: 'sri lanka',
   email: 'j.siri@totalinternet.com',
   discordUsername: 'JS1',
+  modulesCompleted: ['html', 'js', 'css'], // added as part of the challenge
 };
 
 const studentDetailsCopy = {};
@@ -17,3 +18,8 @@ console.log(studentDetailsCopy); // {firstName: 'janaka', lastName: 'siriwardena
 Challenge:
     1. Prove that what we have created here is a shallow copy.
 */
+
+studentDetailsCopy.modulesCompleted[0] = 'react';
+console.log(studentDetails); // {firstName: 'janaka', lastName: 'siriwardena', age: 28, country: 'sri lanka', email: 'j.siri@totalinternet.com', discordUsername: 'JS1', modulesCompleted: ['react', 'js', 'css']}
+console.log(studentDetailsCopy); // {firstName: 'janaka', lastName: 'siriwardena', age: 28, country: 'sri lanka', email: 'j.siri@totalinternet.com', discordUsername: 'JS1', modulesCompleted: ['react', 'js', 'css']}
+// We can see that in both cases above there is 'react' but no sign of 'html'. This proves this is a shallow copy
